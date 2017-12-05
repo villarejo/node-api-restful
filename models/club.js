c7b5421d338cb946d3c4a53a86c5c7a0e7322782
villarejo <j.villarejo.vadillo@gmail.com>
2017-12-05 22:54:56 +0100

    mySQL refactoring2

--- models/club.js
+++ models/club.js
@@ -3,19 +3,35 @@
 const db = require('../db')
 
 function create(userId, text, done) {
-  var values = [userId, text, new Date().toISOString()]
 
-  db.get().query('INSERT INTO comments (user_id, text, date) VALUES(?, ?, ?)', values, function(err, result) {
+	var values = [userId, text, new Date().toISOString()]
+
+	var query = `INSERT INTO Club
+		(nombre,
+		competicionEuropea,
+		supercopa,
+		logo,
+		categoriaActual,
+		categoríaAnterior,
+		economicamenteEstable,
+		anexo_resumen_cpd,
+		padreFilial,
+		pais	)
+		VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+
+  db.get().query(query, values, function(err, result) {
     if (err) return done(err)
     done(null, result.insertId)
   })
 }
 
 function findAll(done) {
-  db.get().query('SELECT * FROM Club;', function (err, rows) {
-    if (err) return done(err)
-    done(null, rows)
-  })
+	var query = `SELECT	* FROM Club;`
+
+	db.get().query(query, function (err, rows) {
+		if (err) return done(err)
+		done(null, rows)
+	})
 }
 
 function findById(id, done) {
@@ -24,7 +40,6 @@ function findById(id, done) {
     done(null, rows)
   })
 }
-
 
 module.exports = {
 	create,
--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,24 @@ const auth = require('../middlewares/auth')
 const api = express.Router()
 
 
+//Beware initial '/' is always needed below in api.get('/...')   !!
+
 //Actions involving players
-//Beware initial "/" is always needed!!
 api.get('/mongodb/player', playerCtrl.getPlayers)
 api.get('/mongodb/player/:playerId', playerCtrl.getPlayer)
 api.post('/mongodb/player', auth, playerCtrl.savePlayer)
 api.put('/mongodb/player/:playerId', auth, playerCtrl.updatePlayer)
 api.delete('/mongodb/player/:playerId', auth, playerCtrl.deletePlayer)
 
-
-//Actions involving users and login
-api.post('/signup', userCtrl.signUp)
-api.post('/signin', userCtrl.signIn)
-api.get('/user', userCtrl.getUsers)
-api.delete('/user/:userId', auth, userCtrl.deleteUser)
-
-
 //Actions involving clubs
 api.get('/mysql/club', clubCtrl.getClubs)
+
+//Actions involving users and login
+api.get('/user', userCtrl.getUsers)
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
+api.delete('/user/:userId', auth, userCtrl.deleteUser)
+
 
 // valid token check
 api.get('/private', auth, (req, res) => {
